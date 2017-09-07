971c33c74354a970a94d7e328e3389786b3a9444
Patcharapon Neranartkomol <patcharaponn@gmail.com>
2017-09-06 22:10:45 -0700

    Front end and routing update

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return render_template("main.html")
+    return render_template("index.html")
 
+@app.route('/about')
+def about():
+	return render_template("about.html")
 
 if __name__ == "__main__":
     app.run()
