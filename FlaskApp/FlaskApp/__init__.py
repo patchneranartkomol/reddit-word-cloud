4e0e708e1db52f43d17cd4312c4ed8595fa7aa0c
Patcharapon Neranartkomol <patcharaponn@gmail.com>
2017-09-14 00:24:28 -0700

    Form handling with WTForms and copy changes

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -1,15 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'TopSecretKey' #TODO: stubbed for testing, move this to ignored config file
 
-@app.route('/')
+class BasicForm(FlaskForm):
+	username = StringField('username')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def homepage():
-    return render_template("index.html")
+	form = BasicForm()
+	return render_template("index.html", form=form)
 
 @app.route('/about')
 def about():
 	return render_template("about.html")
 
 if __name__ == "__main__":
-    app.run()
-      
+	app.run()
+	  
